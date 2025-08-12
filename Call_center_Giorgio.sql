9c1429374607cc59b0f9dc2d29a6ff5ae01f3f36
cargior <cgiorgio18@gmail.com>
2025-08-12 19:55:16 -0300

    1.2.5

---
+++ Call_center_Giorgio.sql
@@ -0,0 +1,57 @@
+drop table nomina;
+
+-- crear base de datos--	
+create database Call_center;
+
+-- seleccionar la BBDD--
+use Call_center;
+
+-- creación de las tablas--
+-- Tabla: Servicios
+CREATE TABLE Servicios (
+    servicio VARCHAR(100) PRIMARY KEY
+);
+
+-- Tabla: Nomina (referencia a Servicios)
+CREATE TABLE Nomina (
+    Legajo INT PRIMARY KEY,
+    usuario VARCHAR(10) NOT NULL UNIQUE,
+    Estado VARCHAR(100) NOT NULL,
+    fecha_ingreso DATE NOT NULL,
+    fecha_de_alta DATE NOT NULL,
+    hora_ingreso TIME NOT NULL,
+    hora_egreso TIME NOT NULL,
+    franco_1 VARCHAR(20),
+    franco_2 VARCHAR(10),
+    servicio VARCHAR(100) NOT NULL,
+    mail VARCHAR(100) NOT NULL,
+    FOREIGN KEY (servicio) REFERENCES Servicios(servicio)
+);
+
+-- Tabla: Planificado (referencia válida)
+CREATE TABLE Planificado (
+    FECHA DATE NOT NULL,
+    FRANJA_HORARIA TIME NOT NULL,
+    SERVICIO VARCHAR(100) NOT NULL,
+    CANTIDAD_DE_PERSONAS INT NOT NULL,
+    PRIMARY KEY (FECHA, FRANJA_HORARIA, SERVICIO),
+    FOREIGN KEY (SERVICIO) REFERENCES Servicios(servicio)
+);
+
+-- Tabla: Requerido
+CREATE TABLE Requerido (
+    FECHA DATE NOT NULL,
+    FRANJA_HORARIA TIME NOT NULL,
+    SERVICIO VARCHAR(100) NOT NULL,
+    CANTIDAD_DE_PERSONAS INT NOT NULL,
+    PRIMARY KEY (FECHA, FRANJA_HORARIA, SERVICIO),
+    FOREIGN KEY (SERVICIO) REFERENCES Servicios(servicio)
+);
+
+-- Tabla: Requerido_servicio
+CREATE TABLE lay_out (
+    SERVICIO VARCHAR(100) PRIMARY KEY,
+    PISO VARCHAR(50) NOT NULL,
+    Q__PA_ASIGNADAS INT NOT NULL,
+    FOREIGN KEY (SERVICIO) REFERENCES Servicios(servicio)
+);
