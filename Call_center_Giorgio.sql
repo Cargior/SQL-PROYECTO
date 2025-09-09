ba9c589275f86380d353955757b2e8ace0dbbece
cargior <cgiorgio18@gmail.com>
2025-09-09 15:49:15 -0300

    1.3.1

--- Call_center_Giorgio.sql
+++ Call_center_Giorgio.sql
@@ -1,5 +1,3 @@
-drop table nomina;
-
 -- crear base de datos--	
 create database Call_center;
 
@@ -85,4 +83,45 @@ CREATE TABLE lay_out (
     PISO VARCHAR(50),
     Q__PA_ASIGNADAS INT,
     FOREIGN KEY (SERVICIO) REFERENCES Servicios(servicio)
-);
+    
+    -- insertar valores servicio --
+
+
+INSERT INTO Servicios (servicio)
+VALUES ('RETENCION CABLE');
+
+-- insertar valores nomina --
+
+INSERT INTO Nomina (Legajo, usuario, Estado, fecha_ingreso, fecha_de_alta, hora_ingreso, hora_egreso, franco_1, franco_2, servicio, mail)
+VALUES
+(202600, 'U561875', 'ACTIVO TELETRABAJO', '2013/04/22', '2013/04/22', '08:30:00', '14:30:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'U561875@CAT-TECHNOLOGIES.COM'),
+(8979, 'U614832', 'ACTIVO ON SITE', '2014/04/07', '2014/04/07', '15:00:00', '21:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'JECANAS@CAT-TECHNOLOGIES.COM'),
+(205849, 'U578398', 'ACTIVO ON SITE', '2015/03/09', '2015/03/09', '08:00:00', '14:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'MATABORDA@CAT-TECHNOLOGIES.COM'),
+(9739, 'U614946', 'ACTIVO TELETRABAJO', '2016/03/01', '2016/03/01', '08:30:00', '14:30:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'DTAPIA@CAT-TECHNOLOGIES.COM'),
+(14898, 'U626769', 'ACTIVO TELETRABAJO', '2019/10/21', '2019/10/21', '08:00:00', '14:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'U626769@CAT-TECHNOLOGIES.COM'),
+(14935, 'U625958', 'ACTIVO ON SITE', '2019/11/04', '2019/11/04', '09:00:00', '15:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'U625958@CAT-TECHNOLOGIES.COM'),
+(16894, 'u921825', 'ACTIVO ON SITE', '2021/12/15', '2021/12/15', '09:00:00', '15:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'gfmaldonado@cat-technologies.com'),
+(17911, 'u925305', 'ACTIVO TELETRABAJO', '2022/04/12', '2022/04/12', '08:00:00', '14:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'mbenitez@cat-technologies.com'),
+(19891, 'u932961', 'ACTIVO TELETRABAJO', '2023/01/23', '2023/01/23', '08:30:00', '14:30:00', 'Domingo', 'Lunes', 'RETENCION CABLE', 'ndiaz@cat-technologies.com'),
+(17988, 'u925873', 'ACTIVO ON SITE', '2022/04/26', '2022/04/26', '09:00:00', '15:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'waleman@cat-technologies.com'),
+(18259, 'u927123', 'ACTIVO TELETRABAJO', '2022/06/23', '2022/06/23', '09:00:00', '15:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'mquiroga@cat-technologies.com'),
+(18640, 'u928572', 'ACTIVO TELETRABAJO', '2022/08/10', '2022/08/10', '09:00:00', '15:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'lmsanchez@cat-technologies.com'),
+(18741, 'u928733', 'ACTIVO TELETRABAJO', '2022/08/24', '2022/08/24', '15:00:00', '20:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'jlgomez@cat-technologies.com'),
+(18999, 'u929678', 'ACTIVO TELETRABAJO', '2022/09/22', '2022/09/22', '08:30:00', '14:30:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'SGUERZOARU17@GMAIL.COM'),
+(19102, 'u930002', 'ACTIVO ON SITE', '2022/10/03', '2022/10/03', '08:00:00', '14:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'fcoria@cat-technologies.com'),
+(19879, 'u932906', 'ACTIVO TELETRABAJO', '2023/01/19', '2023/01/19', '09:00:00', '15:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'gaguirre@cat-technologies.com'),
+(20666, 'u934788', 'ACTIVO ON SITE', '2023/05/02', '2023/05/02', '15:00:00', '21:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', ''),
+(21115, 'u936818', 'ACTIVO TELETRABAJO', '2023/10/19', '2023/10/19', '09:00:00', '15:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', ''),
+(21354, 'u938103', 'ACTIVO TELETRABAJO', '2023/12/20', '2023/12/20', '08:00:00', '14:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'aaavila@cat-technologies.com'),
+(21472, 'u938619', 'ACTIVO TELETRABAJO', '2024/01/02', '2024/01/02', '15:00:00', '21:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'abromero@cat-technologies.com'),
+(21590, 'u939164', 'ACTIVO ON SITE', '2024/01/10', '2024/01/10', '09:00:00', '15:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'laescobar@cat-technologies.com'),
+(21802, 'u939690', 'ACTIVO TELETRABAJO', '2024/01/25', '2024/01/25', '16:00:00', '22:00:00', 'Domingo', 'Viernes', 'RETENCION CABLE', 'fjsantoro@cat-technologies.com'),
+(21810, 'u939700', 'ACTIVO ON SITE', '2024/01/25', '2024/01/25', '16:00:00', '22:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'apboscoscuro@cat-technologies.com'),
+(15357, 'u939746', 'ACTIVO TELETRABAJO', '2020/09/01', '2020/09/01', '08:00:00', '14:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'mcastiglione@cat-technologies.com'),
+(21829, 'u939751', 'ACTIVO TELETRABAJO', '2024/01/25', '2024/01/25', '09:00:00', '15:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'merios@cat-technologies.com'),
+(21823, 'u939771', 'ACTIVO ON SITE', '2024/01/25', '2024/01/25', '15:00:00', '21:00:00', 'Domingo', 'Lunes', 'RETENCION CABLE', 'lmgutierrez@cat-technologies.com'),
+(19795, 'u935107', 'ACTIVO TELETRABAJO', '2023/01/10', '2023/01/10', '08:00:00', '14:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'mjaquet@cat-technologies.com'),
+(23203, 'u402299', 'ACTIVO ON SITE', '2024/12/23', '2024/12/23', '15:00:00', '21:00:00', 'Domingo', 'Martes', 'RETENCION CABLE', ''),
+(22853, 'u401134', 'ACTIVO ON SITE', '2024/11/07', '2024/11/07', '15:00:00', '21:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'nicarbajal@cat-technologies.com'),
+(22858, 'u401139', 'ACTIVO ON SITE', '2024/11/07', '2024/11/07', '15:00:00', '21:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'rslopez@cat-technologies.com'),
+(22862, 'u401143', 'ACTIVO TELETRABAJO', '2024/11/07', '2024/11/07', '16:00:00', '22:00:00', 'Sábado', 'Domingo', 'RETENCION CABLE', 'mamenalled@cat-technologies.com');
